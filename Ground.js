5848bb424255a11f17a06d0a007daab7001cfbc4
Vivaan Raheja <vivaantr08@gmail.com>
2020-08-19 18:31:31 +0530

    initial commit

--- Ground.js
+++ Ground.js
@@ -11,6 +11,7 @@ class Ground {
     display(){
       var pos =this.body.position;
       rectMode(CENTER);
+      stroke("black");
       fill("brown");
       rect(pos.x, pos.y, this.width, this.height);
     }
